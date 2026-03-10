d5a0941a0f7cd12abf10018701f5ebaa211c1550
TEA <toth.ervin.albert@ms.sapientia.ro>
2026-03-10 14:17:02 +0200

    lab4

--- 03.labor/lab3_D.hs
+++ 03.labor/lab3_D.hs
@@ -1,3 +1,6 @@
+import Data.ByteString (foldl', foldr')
+import System.Win32 (xBUTTON1, pAGE_EXECUTE)
+
 -- # 3. labor
 
 -- I. Mit csinálnak az alábbi függvényhívások, ahol az atlag a számok átlagát meghatározó függvény?
@@ -15,17 +18,130 @@ atlag ls = (sum ls) / fromIntegral (length ls)
 -- II. Könyvtárfüggvények használata nélkül írjuk meg azt a Haskell függvényt, amely
 
 -- - meghatározza egy lista elemszámát, 2 módszerrel (myLength),
+
+myLength [] = 0
+myLength (x : xs) = 1 + myLength xs
+
+myLength2 ls = foldr (\_ db -> 1 + db) 0 ls
+
+myLength3 xs = foldr (\x -> (+) 1) 0 xs
+
+myLength4 [] res = res
+myLength4 (x : xs) res = myLength4 xs (res + 1)
+
+myLength5 ls = foldl (\db x -> 1 + db) 0 ls
+
 -- - összeszorozza a lista elemeit, 2 módszerrel (myProduct),
+myProduct [] = 1
+myProduct (x : xs) = x * myProduct xs
+
+myProduct2 [] res = res
+myProduct2 (x : xs) res = myProduct2 xs (res * x)
+
+myProduct3 ls = foldr (*) 1 ls
+
+myProduct4 ls = foldl (*) 1 ls
+
+myProduct5 ls = foldr (*) ls
+
+myProduct6 ls = foldl (*) ls
+
+myProduct7 ls = foldr' (*) 1 ls
+
+myProduct8 ls = foldl' (*) 1 ls
+
 -- - meghatározza egy lista legkisebb elemét (myMinimum),
+
+myMinimum [x] = x
+myMinimum (x1 : x2 : xs) = if x1 < x2 then myMinimum (x1 : xs) else myMinimum (x2 : xs)
+
+myMinimum2 [x] =x
+myMinimum2 (x1 : x2 : xs)
+    | x1 < x2 = myMinimum2 (x1 : xs)
+    | otherwise = myMinimum2 (x2 : xs)
+
+myMinimum3 ls = minimum ls
+
+myMinimum4 ls = foldr1 min ls
+
 -- - meghatározza egy lista legnagyobb elemét (myMaximum),
+myMaximum [x] = x
+myMaximum (x1 : x2 : xs) = if x1 > x2 then myMaximum (x1 : xs) else myMaximum (x2 : xs)
+
+myMaximum2 [x] = x
+myMaximum2 (x1 : x2 :xs)
+    | x1 > x2 = myMaximum2 (x1 : xs)
+    | otherwise = myMaximum2 (x2 : xs)
+
+myMaximum3 ls = foldl1 max ls
+
+myMaximum4 [x] = x
+myMaximum4 (x : xs) = max x (myMaximum xs)
+
 -- - meghatározza egy lista n-ik elemét (!!),
+listaN ls n = ls !! n
+
+listaN2 ls n
+    | ls == [] = error "ures lista"
+    | length ls <= n = error "tul nagy index"
+    | n < 0 = error "negativ index"
+    | otherwise = ls !! n
+
+ls3 = [([1..10], 0), ([5,6,7], 2)]
+
+listaMap = map (uncurry listaN) ls3
+
+listaMap2 = map (x -> listaN x 0) ls3
+
 -- - egymásután fűzi a paraméterként megadott két listát (++),
+listaFuz ls1 ls2 = ls1 ++ ls2
+
+listaFuz2 ls1 ls2 = (++) ls1 ls2
+
+ls4 = [[1..10], [6..15]]
 -- - megállapítja egy listáról, hogy az palindrom-e vagy sem,
+palindrom ls = if ls == reverse ls then "palindrom" else "nem palindrom"
+
+palindrom2 [] = True
+palindrom2 [x] = True
+palindrom2 ls = ( head ls == last ls ) && palindrom2 (init $ tail ls)
+
 -- - meghatározza egy egész szám számjegyeinek listáját,
+szjLs x
+    | x < 10 = [x]
+    | otherwise = szjLs (div x 10) ++ [mod x 10]
+
+szjLs2 x
+    | x < 10 = [x]
+    | otherwise = (mod x 10) : szjLs2 (div x 10)
+
+szjLs2sg x = reverse (szjLs2 x)
+
 -- - a lista első elemét elköltözteti a lista végére,
+elsoutolso (x : xs) = xs ++ [x]
+
+elsoutolso2 xs = tail xs ++[head xs]
+
 -- - meghatározza egy egész elemű lista elemeinek átlagértékét,
+lsAtlag ls = osszeg / hossz
+    where
+        osszeg = sum ls
+        hossz = fromIntegral (length ls)
+
 -- - meghatározza egy 10-es számrendszerbeli szám p számrendszerbeli alakját,
+decP x p
+    | x < p = [x]
+    |otherwise = decP (div x p) p ++ [mod x p]
+
 -- - meghatározza egy p számrendszerben megadott szám számjegyei alapján a megfelelő 10-es számrendszerbeli számot.
+pDec ls p = foldl (\sg x -> sg * p + x) 0 ls
+
+pDec2 x p = [i * (p * hatvany) | (i, hatvany) <- zip (szamjegyek x p) [0 ..]]
+    where
+        szamjegyek x p
+            | x < 10 = [x]
+            | otherwise = mod x 10 : szamjegyek (div x 10) p
+
 
 -- III. Alkalmazzuk a map függvényt a II.-nél megírt függvényekre.
 
